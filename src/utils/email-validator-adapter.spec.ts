7d0998e711ee32236cc6a992852ff201a115205c
Paulo Paiva <paulojr.eco@gmail.com>
2023-07-24 15:51:11 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -24,4 +24,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@mail.com');
     expect(isValid).toBe(true);
   });
+
+  test('Should callvalidator with correct email', () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = vi.spyOn(validator, 'isEmail');
+    sut.isValid('any_email@mail.com');
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
 });
