b2da603f0191818b8dbb2e7490a5305c6af9e468
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-05 17:07:52 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,18 @@
+import { type AddAccountModel } from 'domain/usecases/add-account';
+import { type AddAccountRepository } from 'data/protocols/add-account-repository';
+import { type AccountModel } from 'domain/models/account';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.insertOne(accountData);
+    const result = await accountCollection.findOne(accountData);
+    if (result) {
+      const { _id, name, email, password } = result;
+      return { id: _id.toString(), name, email, password };
+    } else {
+      throw new Error();
+    }
+  }
+}
