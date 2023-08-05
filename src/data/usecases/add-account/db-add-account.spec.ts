c2014e644521391e86aaf6d6f1804bd3381e5cb9
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-04 23:21:41 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -95,4 +95,20 @@ describe('DBAddAccount Usecase', () => {
       password: 'hashed_password'
     });
   });
+
+  test('Shoud throw if Repository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    vi.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error());
+      })
+    );
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    };
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  });
 });
