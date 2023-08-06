b3e76042ec67fd477d3bac6657e6883b86b3df26
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-06 00:04:02 -0300

    chore: remove main folder from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   test: {
     globals: true,
     coverage: {
-      provider: 'istanbul'
+      provider: 'istanbul',
+      exclude: ['**/main/**']
     }
   },
   plugins: [tsconfigPaths()]
