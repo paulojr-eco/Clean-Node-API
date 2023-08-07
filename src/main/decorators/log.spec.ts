5a758d631ea3200745c9c368e394a4b63bec5474
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-06 23:45:23 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -4,11 +4,8 @@ import {
   type HttpRequest
 } from 'presentation/protocols';
 import { LogControllerDecorator } from './log';
-
-interface SutTypes {
-  sut: LogControllerDecorator
-  controllerStub: Controller
-}
+import { serverError } from 'presentation/helpers/http-helper';
+import { type LogErrorRepository } from 'data/protocols/log-error-repository';
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
@@ -25,12 +22,27 @@ const makeController = (): Controller => {
   return new ControllerStub();
 };
 
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async log (stack: string): Promise<void> {}
+  }
+  return new LogErrorRepositoryStub();
+};
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+  logErrorRepositoryStub: LogErrorRepository
+}
+
 const makeSut = (): SutTypes => {
   const controllerStub = makeController();
-  const sut = new LogControllerDecorator(controllerStub);
+  const logErrorRepositoryStub = makeLogErrorRepository();
+  const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub);
   return {
     sut,
-    controllerStub
+    controllerStub,
+    logErrorRepositoryStub
   };
 };
 
@@ -65,5 +77,28 @@ describe('LogController Decorator', () => {
       statusCode: 200,
       body: {}
     });
+  });
+
+  test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
+    const fakeError = new Error();
+    fakeError.stack = 'any_stack';
+    const error = serverError(fakeError);
+    const logPsy = vi.spyOn(logErrorRepositoryStub, 'log');
+    vi.spyOn(controllerStub, 'handle').mockReturnValueOnce(
+      new Promise((resolve) => {
+        resolve(error);
+      })
+    );
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    await sut.handle(httpRequest);
+    expect(logPsy).toHaveBeenCalledWith('any_stack');
   });
 });
