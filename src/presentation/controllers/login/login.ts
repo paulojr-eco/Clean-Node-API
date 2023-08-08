4a9136b7d12c974eb02b2e0a20d8606c7fe35cb2
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-07 23:21:11 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,4 +1,8 @@
-import { badRequest, serverError, successful } from 'presentation/helpers/http-helper';
+import {
+  badRequest,
+  serverError,
+  successful
+} from 'presentation/helpers/http-helper';
 import {
   type EmailValidator,
   type Controller,
@@ -18,27 +22,19 @@ export class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredField = ['email', 'password'];
+      for (const field of requiredField) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
       const { email, password } = httpRequest.body;
-      if (!email) {
-        return await new Promise((resolve) => {
-          resolve(badRequest(new MissingParamError('email')));
-        });
-      }
-      if (!password) {
-        return await new Promise((resolve) => {
-          resolve(badRequest(new MissingParamError('password')));
-        });
-      }
       const isValid = this.emailValidator.isValid(email);
       if (!isValid) {
-        return await new Promise((resolve) => {
-          resolve(badRequest(new InvalidParamError('email')));
-        });
+        return badRequest(new InvalidParamError('email'));
       }
       await this.authentication.auth(email, password);
-      return await new Promise((resolve) => {
-        resolve(successful(''));
-      });
+      return successful('');
     } catch (error) {
       console.error(error);
       if (error instanceof Error) {
