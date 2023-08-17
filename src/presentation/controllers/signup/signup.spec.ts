38edfa66383fb408cb550bfacb73f60c53fe0c31
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-16 21:39:34 -0300

    test: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -9,7 +9,8 @@ import {
   type AddAccount,
   type AddAccountModel,
   type AccountModel,
-  type HttpRequest
+  type HttpRequest,
+  type Validation
 } from './signup-protocols';
 
 const makeEmailValidator = (): EmailValidator => {
@@ -38,6 +39,15 @@ const makeAddAccount = (): AddAccount => {
   return new AddAccountStub();
 };
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null;
+    }
+  }
+  return new ValidationStub();
+};
+
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     name: 'any_name',
@@ -51,16 +61,19 @@ interface SutTypes {
   sut: SignUpController
   emailValidatorStub: EmailValidator
   addAccountStub: AddAccount
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator();
   const addAccountStub = makeAddAccount();
-  const sut = new SignUpController(emailValidatorStub, addAccountStub);
+  const validationStub = makeValidation();
+  const sut = new SignUpController(emailValidatorStub, addAccountStub, validationStub);
   return {
     sut,
     emailValidatorStub,
-    addAccountStub
+    addAccountStub,
+    validationStub
   };
 };
 
@@ -215,5 +228,13 @@ describe('SignUp Controller', () => {
       email: 'valid_email@mail.com',
       password: 'valid_password'
     });
+  });
+
+  test('Should call Vialidation with correct value', async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = vi.spyOn(validationStub, 'validate');
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 });
