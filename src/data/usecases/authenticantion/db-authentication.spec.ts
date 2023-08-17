ba02d6ff6ff899e543db183485b5c326eb42efe2
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-17 18:08:39 -0300

    test: ensure DbAuthentication throws if LoadAccountByEmailRepository throws

--- src/data/usecases/authenticantion/db-authentication.spec.ts
+++ src/data/usecases/authenticantion/db-authentication.spec.ts
@@ -48,4 +48,13 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication());
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
+
+  test('Should throw if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    vi.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(
+      Promise.reject(new Error())
+    );
+    const promise = sut.auth(makeFakeAuthentication());
+    await expect(promise).rejects.toThrow();
+  });
 });
