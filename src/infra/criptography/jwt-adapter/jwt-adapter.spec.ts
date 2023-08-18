0d7963eadb15946a8784619c7f225d6e9d3182c4
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-18 13:29:15 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,19 +2,28 @@ import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
 
 describe('Jwt Adapter', () => {
-  test('Should call sign with correct values', async () => {
+  test('Should call sign with correct values', () => {
     const sut = new JwtAdapter('secret');
     const signSpy = vi.spyOn(jwt, 'sign');
     sut.encrypt('any_id');
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
   });
 
-  test('Should return a token on sign success', async () => {
+  test('Should return a token on sign success', () => {
     const sut = new JwtAdapter('secret');
     vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       return 'any_token';
     });
     const accessToken = sut.encrypt('any_id');
     expect(accessToken).toBe('any_token');
+  });
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret');
+    vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const encryptCall = (): string => sut.encrypt('any_id');
+    expect(encryptCall).toThrowError();
   });
 });
