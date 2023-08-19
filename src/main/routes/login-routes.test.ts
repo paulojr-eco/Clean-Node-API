5803d2c0635cf844e096ebeca139c25ee2c93461
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-19 06:34:04 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from 'main/config/app';
 import { MongoHelper } from 'infra/db/mongodb/helpers/mongo-helper';
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   const cleanCollection = async (collectionName: string): Promise<void> => {
     const collection = await MongoHelper.getCollection(collectionName);
     await collection.deleteMany({});
@@ -20,18 +20,20 @@ describe('SignUp Routes', () => {
     await cleanCollection('accounts');
   });
 
-  test('Should return an account on success', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    setTimeout(async () => {
-      await request(app)
-        .post('/api/signup')
-        .send({
-          name: 'Paulo',
-          email: 'paulo@mail.com',
-          password: '123',
-          passwordConfirmation: '123'
-        })
-        .expect(200);
-    }, 3000);
+  describe('/POST signup', () => {
+    test('Should return 200 on signup', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      setTimeout(async () => {
+        await request(app)
+          .post('/api/signup')
+          .send({
+            name: 'Paulo',
+            email: 'paulo@mail.com',
+            password: '123',
+            passwordConfirmation: '123'
+          })
+          .expect(200);
+      }, 3000);
+    });
   });
 });
