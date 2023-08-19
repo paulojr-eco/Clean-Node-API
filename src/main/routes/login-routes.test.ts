48c3e3415c170f21b62390be687ac185fa05797e
Paulo Paiva <paulojr.eco@gmail.com>
2023-08-19 07:26:45 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,11 +1,15 @@
 import request from 'supertest';
 import app from 'main/config/app';
 import { MongoHelper } from 'infra/db/mongodb/helpers/mongo-helper';
+import { type Collection } from 'mongodb';
+import { hash } from 'bcrypt';
+
+let accountCollection: Collection;
 
 describe('Login Routes', () => {
   const cleanCollection = async (collectionName: string): Promise<void> => {
-    const collection = await MongoHelper.getCollection(collectionName);
-    await collection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection(collectionName);
+    await accountCollection.deleteMany({});
   };
   beforeAll(async () => {
     await MongoHelper.connect();
@@ -34,6 +38,25 @@ describe('Login Routes', () => {
           })
           .expect(200);
       }, 3000);
+    });
+  });
+
+  describe('/POST login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12);
+      await accountCollection.insertOne({
+        name: 'Paulo',
+        email: 'paulo@mail.com',
+        password
+      });
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'paulo@mail.com',
+          password: '123'
+        })
+        .expect(200);
     });
   });
 });
