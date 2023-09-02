d7fd25a9247c352f7b202a85a457160d27bb8774
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-02 16:22:35 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,16 +3,19 @@ import {
   type AddAccountModel,
   type AccountModel,
   type Hasher,
-  type AddAccountRepository
+  type AddAccountRepository,
+  type LoadAccountByEmailRepository
 } from './ad-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) { }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
     const hashedPassword = await this.hasher.hash(accountData.password);
     const account = await this.addAccountRepository.add({
       ...accountData,
--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -3,7 +3,7 @@ import { type Controller } from '../../../../presentation/protocols';
 import { makeSignUpValidation } from './signup-validation-factory';
 import { makeDbAuthentication } from '../../../../main/factories/usecases/authentication/db-authentication-factory';
 import { makeDbAddAccount } from '../../../../main/factories/usecases/add-account/db-add-account-factory';
-import { makeLogControllerDecorator } from 'main/factories/decorators/log-controller-decorator-factory';
+import { makeLogControllerDecorator } from '../../../../main/factories/decorators/log-controller-decorator-factory';
 
 export const makeSignUpController = (): Controller => {
   const signUpController = new SignUpController(
