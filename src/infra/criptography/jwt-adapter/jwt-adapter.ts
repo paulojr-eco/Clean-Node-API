a54a45d33b28632b2053012c7503085d49937cbc
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-02 21:02:20 -0300

    refactor: standardize imports in infra layer

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { type Encrypter } from 'data/protocols/criptography/encrypter';
+import { type Encrypter } from '../../../data/protocols/criptography/encrypter';
 
 export class JwtAdapter implements Encrypter {
   constructor (private readonly secret: string) {
--- src/infra/db/mongodb/account/account-mongo-respoitory.ts
+++ src/infra/db/mongodb/account/account-mongo-respoitory.ts
@@ -1,8 +1,8 @@
-import { type AddAccountModel } from 'domain/usecases/add-account';
-import { type AddAccountRepository } from 'data/protocols/db/account/add-account-repository';
-import { type AccountModel } from 'domain/models/account';
+import { type AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository';
+import { type AddAccountModel } from '../../../../domain/usecases/add-account';
+import { type AccountModel } from '../../../../domain/models/account';
+import { type LoadAccountByEmailRepository, type UpdateAccessTokenRepository } from '../../../../data/usecases/authenticantion/db-authentication-protocols';
 import { MongoHelper } from '../helpers/mongo-helper';
-import { type LoadAccountByEmailRepository, type UpdateAccessTokenRepository } from 'data/usecases/authenticantion/db-authentication-protocols';
 import { ObjectId } from 'mongodb';
 
 export class AccountMongoRepository
