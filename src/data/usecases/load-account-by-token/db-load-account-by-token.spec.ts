20d7b40b09621c600de6d352b30ae4f18229a2c8
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-07 16:55:12 -0300

    test: ensure DbLoadAccountByToken calls Decrypter with correct value

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -0,0 +1,34 @@
+import { type Decrypter } from 'data/protocols/criptography/decrypter';
+import { DbLoadAccountByToken } from './db-load-account-by-token';
+
+interface SutTypes {
+  sut: DbLoadAccountByToken
+  decrypterStub: Decrypter
+}
+
+const makeDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    decrypt (value: string): string {
+      return 'any_value';
+    }
+  }
+  return new DecrypterStub();
+};
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypter();
+  const sut = new DbLoadAccountByToken(decrypterStub);
+  return {
+    sut,
+    decrypterStub
+  };
+};
+
+describe('DbLoadAccountByToken Usecase', () => {
+  test('Shoudl call Decrypter with correct values', async () => {
+    const { sut, decrypterStub } = makeSut();
+    const decryptSpy = vi.spyOn(decrypterStub, 'decrypt');
+    await sut.load('any_token');
+    expect(decryptSpy).toHaveBeenCalledWith('any_token');
+  });
+});
