a2559637abc493721910ce70925473905d8ea47b
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-07 19:07:15 -0300

    refactor: separate tests in describe blocks

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -7,60 +7,69 @@ const makeSut = (): BcryptAdapter => {
 };
 
 describe('Bcrypt Adapter', () => {
-  test('Should call hash with correct values', async () => {
-    const sut = makeSut();
-    const hashSpy = vi.spyOn(bcrypt, 'hash');
-    await sut.hash('any_value');
-    expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
-  });
-
-  test('Should return a valid hash on hash success', async () => {
-    const sut = makeSut();
-    vi.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
-      return await new Promise(resolve => { resolve('hash'); });
+  describe('hash()', () => {
+    test('Should call hash with correct values', async () => {
+      const sut = makeSut();
+      const hashSpy = vi.spyOn(bcrypt, 'hash');
+      await sut.hash('any_value');
+      expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
     });
-    const hash = await sut.hash('any_value');
-    expect(hash).toBe('hash');
-  });
 
-  test('Should throw if hash throws', async () => {
-    const sut = makeSut();
-    vi.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
-      await Promise.reject(new Error());
+    test('Should return a valid hash on hash success', async () => {
+      const sut = makeSut();
+      vi.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
+        return await new Promise((resolve) => {
+          resolve('hash');
+        });
+      });
+      const hash = await sut.hash('any_value');
+      expect(hash).toBe('hash');
     });
-    const promise = sut.hash('any_value');
-    await expect(promise).rejects.toThrow();
-  });
 
-  test('Should call compare with correct values', async () => {
-    const sut = makeSut();
-    const compareSpy = vi.spyOn(bcrypt, 'compare');
-    await sut.compare('any_value', 'any_hash');
-    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash');
-  });
-
-  test('Should return true when compare succeeds', async () => {
-    const sut = makeSut();
-    vi.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => {
-      return await new Promise(resolve => { resolve(true); });
+    test('Should throw if hash throws', async () => {
+      const sut = makeSut();
+      vi.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
+        await Promise.reject(new Error());
+      });
+      const promise = sut.hash('any_value');
+      await expect(promise).rejects.toThrow();
     });
-    const isValid = await sut.compare('any_value', 'any_hash');
-    expect(isValid).toBe(true);
   });
-
-  test('Should return false when compare fails', async () => {
-    const sut = makeSut();
-    vi.spyOn(bcrypt, 'compare').mockReturnValueOnce(Promise.resolve(false) as any);
-    const isValid = await sut.compare('any_value', 'any_hash');
-    expect(isValid).toBe(false);
-  });
-
-  test('Should throw if compare throws', async () => {
-    const sut = makeSut();
-    vi.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => {
-      await Promise.reject(new Error());
+  describe('compare()', () => {
+    test('Should call compare with correct values', async () => {
+      const sut = makeSut();
+      const compareSpy = vi.spyOn(bcrypt, 'compare');
+      await sut.compare('any_value', 'any_hash');
+      expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash');
     });
-    const promise = sut.compare('any_value', 'any_hash');
-    await expect(promise).rejects.toThrow();
+
+    test('Should return true when compare succeeds', async () => {
+      const sut = makeSut();
+      vi.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => {
+        return await new Promise((resolve) => {
+          resolve(true);
+        });
+      });
+      const isValid = await sut.compare('any_value', 'any_hash');
+      expect(isValid).toBe(true);
+    });
+
+    test('Should return false when compare fails', async () => {
+      const sut = makeSut();
+      vi.spyOn(bcrypt, 'compare').mockReturnValueOnce(
+        Promise.resolve(false) as any
+      );
+      const isValid = await sut.compare('any_value', 'any_hash');
+      expect(isValid).toBe(false);
+    });
+
+    test('Should throw if compare throws', async () => {
+      const sut = makeSut();
+      vi.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => {
+        await Promise.reject(new Error());
+      });
+      const promise = sut.compare('any_value', 'any_hash');
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -6,28 +6,30 @@ const makeSut = (): JwtAdapter => {
 };
 
 describe('Jwt Adapter', () => {
-  test('Should call sign with correct values', () => {
-    const sut = makeSut();
-    const signSpy = vi.spyOn(jwt, 'sign');
-    sut.encrypt('any_id');
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
-  });
-
-  test('Should return a token on sign success', () => {
-    const sut = makeSut();
-    vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-      return 'any_token';
+  describe('sign()', () => {
+    test('Should call sign with correct values', () => {
+      const sut = makeSut();
+      const signSpy = vi.spyOn(jwt, 'sign');
+      sut.encrypt('any_id');
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
     });
-    const accessToken = sut.encrypt('any_id');
-    expect(accessToken).toBe('any_token');
-  });
 
-  test('Should throw if sign throws', async () => {
-    const sut = makeSut();
-    vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-      throw new Error();
+    test('Should return a token on sign success', () => {
+      const sut = makeSut();
+      vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        return 'any_token';
+      });
+      const accessToken = sut.encrypt('any_id');
+      expect(accessToken).toBe('any_token');
     });
-    const encryptCall = (): string => sut.encrypt('any_id');
-    expect(encryptCall).toThrowError();
+
+    test('Should throw if sign throws', async () => {
+      const sut = makeSut();
+      vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error();
+      });
+      const encryptCall = (): string => sut.encrypt('any_id');
+      expect(encryptCall).toThrowError();
+    });
   });
 });
