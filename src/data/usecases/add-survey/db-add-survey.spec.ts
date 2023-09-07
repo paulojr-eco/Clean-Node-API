44de6e4ee587f1e3058471b53a957bf4aae1cac4
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-07 10:35:43 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -1,5 +1,8 @@
 import { DbAddSurvey } from './db-add-survey';
-import { type AddSurveyRepository, type AddSurveyModel } from './db-add-survey-protocols';
+import {
+  type AddSurveyRepository,
+  type AddSurveyModel
+} from './db-add-survey-protocols';
 
 const makeFakeSurveyData = (): AddSurveyModel => ({
   question: 'any_question',
@@ -11,16 +14,35 @@ const makeFakeSurveyData = (): AddSurveyModel => ({
   ]
 });
 
+interface SutTypes {
+  sut: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepository
+}
+
+const makeAddSurveyRepository = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add (surveyData: AddSurveyModel): Promise<void> {
+      await new Promise<void>((resolve) => {
+        resolve();
+      });
+    }
+  }
+  return new AddSurveyRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = makeAddSurveyRepository();
+  const sut = new DbAddSurvey(addSurveyRepositoryStub);
+  return {
+    sut,
+    addSurveyRepositoryStub
+  };
+};
+
 describe('DbAddSurvey UseCase', () => {
   test('Should call AddSurveyRepository with correct values', async () => {
-    class AddSurveyRepositoryStub implements AddSurveyRepository {
-      async add (surveyData: AddSurveyModel): Promise<void> {
-        await new Promise<void>(resolve => { resolve(); });
-      }
-    }
-    const addSurveyRepositoryStub = new AddSurveyRepositoryStub();
+    const { sut, addSurveyRepositoryStub } = makeSut();
     const addSpy = vi.spyOn(addSurveyRepositoryStub, 'add');
-    const sut = new DbAddSurvey(addSurveyRepositoryStub);
     const surveyData = makeFakeSurveyData();
     await sut.add(surveyData);
     expect(addSpy).toHaveBeenCalledWith(surveyData);
