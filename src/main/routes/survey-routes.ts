2df78a3b4a69fad4dcbf529a9bcff929cd7841a5
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-08 15:56:00 -0300

    fix: change import main layer on surveys route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -2,7 +2,7 @@ import { type Router } from 'express';
 import { adaptRoute } from '../adapters/express-route-adapter';
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory';
 import { makeAuthMiddleware } from '../../main/factories/middlewares/auth-middleware-factory';
-import { adaptMiddleware } from 'main/adapters/express-middleware-adapter';
+import { adaptMiddleware } from '../../main/adapters/express-middleware-adapter';
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'));
