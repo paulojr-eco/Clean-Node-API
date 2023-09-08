55a4d86964243eb9935da415f84ee492b3aa36a9
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-08 10:58:54 -0300

    refactor: organize main layer

--- src/main/factories/controllers/login/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -1,8 +1,8 @@
 import { SignUpController } from '../../../../../presentation/controllers/login/signup/signup-controller';
 import { type Controller } from '../../../../../presentation/protocols';
 import { makeSignUpValidation } from './signup-validation-factory';
-import { makeDbAuthentication } from '../../../../../main/factories/usecases/authentication/db-authentication-factory';
-import { makeDbAddAccount } from '../../../../../main/factories/usecases/add-account/db-add-account-factory';
+import { makeDbAuthentication } from '../../../usecases/account/authentication/db-authentication-factory';
+import { makeDbAddAccount } from '../../../usecases/account/add-account/db-add-account-factory';
 import { makeLogControllerDecorator } from '../../../../../main/factories/decorators/log-controller-decorator-factory';
 
 export const makeSignUpController = (): Controller => {
---
+++ src/main/factories/usecases/account/authentication/db-authentication-factory.ts
@@ -0,0 +1,19 @@
+import env from '../../../../config/env';
+import { DbAuthentication } from '../../../../../data/usecases/authenticantion/db-authentication';
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-respoitory';
+import { BcryptAdapter } from '../../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter';
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter';
+import { type Authentication } from '../../../../../domain/usecases/authentication';
+
+export const makeDbAuthentication = (): Authentication => {
+  const salt = 12;
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const jwtAdapter = new JwtAdapter(env.jwtSecret);
+  const accountMongoRepository = new AccountMongoRepository();
+  return new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  );
+};
