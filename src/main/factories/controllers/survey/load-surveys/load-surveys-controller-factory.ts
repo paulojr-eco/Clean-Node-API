6964d83c23f0e5606ebc2b52c69bacfc3fa089d0
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-11 15:01:26 -0300

    feat: ensure load surveys route returns 403 if no acess token is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -0,0 +1,9 @@
+import { type Controller } from '../../../../../presentation/protocols';
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory';
+import { LoadSurveysController } from '../../../../../presentation/controllers/survey/load-survey/load-survey-controller';
+import { makeDbLoadSurveys } from 'main/factories/usecases/survey/load-surveys/db-add-survey-factory';
+
+export const makeLoadSurveysController = (): Controller => {
+  const controller = new LoadSurveysController(makeDbLoadSurveys());
+  return makeLogControllerDecorator(controller);
+};
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,9 +3,12 @@ import { adaptRoute } from '../adapters/express-route-adapter';
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory';
 import { makeAuthMiddleware } from '../../main/factories/middlewares/auth-middleware-factory';
 import { adaptMiddleware } from '../../main/adapters/express-middleware-adapter';
+import { makeLoadSurveysController } from '../../main/factories/controllers/survey/load-surveys/load-surveys-controller-factory';
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'));
+  const auth = adaptMiddleware(makeAuthMiddleware());
   /* eslint-disable @typescript-eslint/no-misused-promises */
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()));
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()));
 };
