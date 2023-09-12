bb61a3e432bd91bb30e1a6a546b9696010af25d0
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-12 15:56:29 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,56 @@
+import { DbSaveSurveyResult } from './db-save-survey-result';
+import { type SaveSurveyResultModel, type SurveyResultModel, type SaveSurveyResultRepository } from './db-save-survey-result-protocols';
+import MockDate from 'mockdate';
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_answer',
+  date: new Date()
+});
+
+const makeFakeSurveyResult = (): SurveyResultModel =>
+  Object.assign({}, makeFakeSurveyResultData(), { id: 'any_id' });
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+};
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return await new Promise((resolve) => {
+        resolve(makeFakeSurveyResult());
+      });
+    }
+  }
+  return new SaveSurveyResultRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository();
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub);
+  return {
+    sut,
+    saveSurveyResultRepositoryStub
+  };
+};
+
+describe('DbSaveSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test('Should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut();
+    const saveSpy = vi.spyOn(saveSurveyResultRepositoryStub, 'save');
+    const surveyResultData = makeFakeSurveyResultData();
+    await sut.save(surveyResultData);
+    expect(saveSpy).toHaveBeenCalledWith(surveyResultData);
+  });
+});
