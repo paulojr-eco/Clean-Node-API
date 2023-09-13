dbb763b033a0d3c8c0c403c90c1d26992ffe6712
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-13 14:31:39 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,28 +1,49 @@
-import { forbidden, serverError, successful } from '@/presentation/helpers/http/http-helper';
+import {
+  forbidden,
+  serverError,
+  successful
+} from '@/presentation/helpers/http/http-helper';
 import {
   type LoadSurveyById,
   type Controller,
   type HttpRequest,
-  type HttpResponse
+  type HttpResponse,
+  type SaveSurveyResult
 } from './save-survey-result-controller-protocols';
 import { InvalidParamError } from '@/presentation/errors';
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params;
       const { answer } = httpRequest.body;
+      const { accountId } = httpRequest;
       const survey = await this.loadSurveyById.loadById(surveyId);
       if (survey) {
-        const answers = survey.answers.map(item => item.answer);
+        const answers = survey.answers.map((item) => item.answer);
         if (!answers.includes(answer)) {
           return forbidden(new InvalidParamError('answer'));
         }
       } else {
         return forbidden(new InvalidParamError('surveyId'));
       }
+
+      if (accountId) {
+        await this.saveSurveyResult.save({
+          accountId,
+          surveyId,
+          answer,
+          date: new Date()
+        });
+      } else {
+        return forbidden(new InvalidParamError('accountId'));
+      }
+
       return successful({});
     } catch (error) {
       console.error(error);
