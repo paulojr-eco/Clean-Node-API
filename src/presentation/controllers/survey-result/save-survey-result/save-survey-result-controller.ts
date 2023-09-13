3056972a31295a3a1feb564703a171dd91452f5e
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-13 13:33:08 -0300

    feat: ensure SaveSurveyResultController throws if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,4 +1,4 @@
-import { forbidden, successful } from '@/presentation/helpers/http/http-helper';
+import { forbidden, serverError, successful } from '@/presentation/helpers/http/http-helper';
 import {
   type LoadSurveyById,
   type Controller,
@@ -11,11 +11,19 @@ export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { params } = httpRequest;
-    const survey = await this.loadSurveyById.loadById(params.surveyId);
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'));
+    try {
+      const { params } = httpRequest;
+      const survey = await this.loadSurveyById.loadById(params.surveyId);
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'));
+      }
+      return successful({});
+    } catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        return serverError(error);
+      }
+      return serverError(new Error('Error while handle save survey result'));
     }
-    return successful({});
   }
 }
