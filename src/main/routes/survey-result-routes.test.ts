b287929c41a849ba1f126b9c531e51b20d0db5fa
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-14 18:15:19 -0300

    test: save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,14 +1,52 @@
 import request from 'supertest';
 import app from '@/main/config/app';
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import { type Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '@/main/config/env';
 
-describe('Survey Result Routes', () => {
+let surveyCollection: Collection;
+let accountCollection: Collection;
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Paulo',
+    email: 'paulo@mail.com',
+    password: '123'
+  });
+  const id = res.insertedId;
+  const accessToken = sign({ id }, env.jwtSecret);
+  await accountCollection.updateOne(
+    {
+      _id: id
+    },
+    {
+      $set: {
+        accessToken
+      }
+    }
+  );
+  return accessToken;
+};
+
+describe('Survey Routes', () => {
+  const resetCollections = async (): Promise<void> => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await surveyCollection.deleteMany({});
+    await accountCollection.deleteMany({});
+  };
   beforeAll(async () => {
     await MongoHelper.connect();
   });
 
   afterAll(async () => {
+    await resetCollections();
     await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    await resetCollections();
   });
 
   describe('PUT /surveys/:surveyId/results', () => {
@@ -21,5 +59,29 @@ describe('Survey Result Routes', () => {
         })
         .expect(403);
     }, 10000);
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken();
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }
+        ],
+        date: new Date()
+      });
+      await request(app)
+        .put(`/api/surveys/${res.insertedId.toString()}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200);
+    });
   });
 });
