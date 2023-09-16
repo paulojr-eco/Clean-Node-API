60064dcf547a5f8c7f17c90e5a178816c815fd5f
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-16 14:58:14 -0300

    docs: add load survey api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
-import { loginPath } from './paths/login-path';
-import { badRequest, serverError, unauthorized } from './components';
-import { accountSchema, errorSchema, loginParamsSchema } from './schemas';
+import { loginPath, surveyPath } from './paths';
+import { badRequest, serverError, unauthorized, forbidden } from './components';
+import { accountSchema, errorSchema, loginParamsSchema, surveyAnswerSchema, surveySchema, surveysSchema, apiKeyAuth } from './schemas';
 
 export default {
   openapi: '3.0.0',
@@ -22,19 +22,30 @@ export default {
   tags: [
     {
       name: 'Login'
+    },
+    {
+      name: 'Enquetes'
     }
   ],
   paths: {
-    '/login': loginPath
+    '/login': loginPath,
+    '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
-    error: errorSchema
+    error: errorSchema,
+    surveys: surveysSchema,
+    survey: surveySchema,
+    surveyAnswer: surveyAnswerSchema
   },
   components: {
+    securitySchemes: {
+      apiKeyAuth
+    },
     badRequest,
     serverError,
-    unauthorized
+    unauthorized,
+    forbidden
   }
 };
---
+++ src/main/docs/paths/survey-path.ts
@@ -0,0 +1,27 @@
+export const surveyPath = {
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para listar todas as enquetes',
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveys'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+};
