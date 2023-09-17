ba7e685ac5848dec4bcdc4870f2dc1552629839a
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-17 17:04:30 -0300

    test: ensure DbLoadSurveyResult throws if LoadSurveyResultRepository throws

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -26,4 +26,16 @@ describe('DbLoadSurveyResult UseCase', () => {
     await sut.load('any_survey_id');
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id');
   });
+
+  test('Should throw if LoadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut();
+    vi.spyOn(
+      loadSurveyResultRepositoryStub,
+      'loadBySurveyId'
+    ).mockReturnValueOnce(
+      Promise.reject(new Error())
+    );
+    const promise = sut.load('any_survey_id');
+    await expect(promise).rejects.toThrow();
+  });
 });
