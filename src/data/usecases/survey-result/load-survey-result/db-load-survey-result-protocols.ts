adbca147bd111f451155e5d805689429b79ba773
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-17 17:01:45 -0300

    refactor: move LoadSurveyResult dependencies to a protocols file

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
@@ -0,0 +1,3 @@
+export * from '@/domain/usecases/survey-result/load-survey-result';
+export * from '../save-survey-result/db-save-survey-result-protocols';
+export * from '@/data/protocols/db/survey-result/load-survey-result-repository';
--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,6 +1,4 @@
-import { type LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
-import { type SurveyResultModel } from '../save-survey-result/db-save-survey-result-protocols';
-import { type LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
+import { type LoadSurveyResult, type LoadSurveyResultRepository, type SurveyResultModel } from './db-load-survey-result-protocols';
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
