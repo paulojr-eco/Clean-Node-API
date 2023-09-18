4d6654c10bb04bb2e57b6ede2ee37657343b473e
Paulo Paiva <paulojr.eco@gmail.com>
2023-09-17 23:25:36 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,6 +1,20 @@
 import { LoadSurveyResultController } from './load-survey-result-controller';
-import { type HttpRequest } from './load-survey-result-controller-protocols';
+import { type LoadSurveyById, type HttpRequest } from './load-survey-result-controller-protocols';
 import { mockLoadSurveyById } from '@/data/test';
+
+type SutTypes = {
+  sut: LoadSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById();
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub);
+  return {
+    sut,
+    loadSurveyByIdStub
+  };
+};
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -10,8 +24,7 @@ const makeFakeRequest = (): HttpRequest => ({
 
 describe('LoadSurveyResult Controller', () => {
   test('Should call LoadSurveyById with correct value', async () => {
-    const loadSurveyByIdStub = mockLoadSurveyById();
-    const sut = new LoadSurveyResultController(loadSurveyByIdStub);
+    const { sut, loadSurveyByIdStub } = makeSut();
     const loadByIdSpy = vi.spyOn(loadSurveyByIdStub, 'loadById');
     await sut.handle(makeFakeRequest());
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id');
